326a03fa3c31059a62a501b5b98a8322fadc1267
Dimitrios <pcmagas@disroot.org>
2021-03-27 17:00:31 +0200

    Add Instructions Layout

---
+++ app/src/main/java/com/example/vodafone_fu_h300s/screens/ConnectIntoRouterActivity.java
@@ -0,0 +1,14 @@
+package com.example.vodafone_fu_h300s.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConnectIntoRouterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_into_router);
+    }
+}
--- app/src/main/java/com/example/vodafone_fu_h300s/screens/InstructionsActivity.java
+++ app/src/main/java/com/example/vodafone_fu_h300s/screens/InstructionsActivity.java
@@ -3,6 +3,7 @@ package com.example.vodafone_fu_h300s.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.vodafone_fu_h300s.R;
 
@@ -11,6 +12,14 @@ public class InstructionsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connection_into_router);
+        setContentView(R.layout.activity_connection_instructions);
+        ImageView img = (ImageView)findViewById(R.id.instructions_image);
+
+        int imageResource = getResources().getIdentifier("@drawable/h300s",null,this.getPackageName());
+        img.setImageResource(imageResource);
+    }
+
+    public void connectionForm(){
+
     }
 }

c4ccd22b69ece6c46d4df6d59353db46fefe1014
Dimitrios <pcmagas@disroot.org>
2021-04-23 23:33:09 +0300

    Detect Ip Screen

--- app/src/main/java/pc_magas/vodafone_fu_h300s/screens/MainScreen.java
+++ app/src/main/java/pc_magas/vodafone_fu_h300s/screens/MainScreen.java
@@ -2,8 +2,11 @@ package pc_magas.vodafone_fu_h300s.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import pc_magas.vodafone_fu_h300s.R;
 
@@ -13,5 +16,16 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        Intent detectIp = new Intent(this,DetectGateWayIPActivity.class);
+        Button retrieve = (Button)findViewById(R.id.retrieve);
+        retrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(detectIp);
+            }
+        });
+
+
     }
 }

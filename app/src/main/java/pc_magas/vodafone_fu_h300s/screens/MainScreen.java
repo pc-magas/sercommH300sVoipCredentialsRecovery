d139f44a42e0bcc1b11f4c8c51e0acc01887c9a9
Dimitrios <pcmagas@disroot.org>
2021-04-23 23:28:19 +0300

    Main Screen

---
+++ app/src/main/java/pc_magas/vodafone_fu_h300s/screens/MainScreen.java
@@ -0,0 +1,17 @@
+package pc_magas.vodafone_fu_h300s.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import pc_magas.vodafone_fu_h300s.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}

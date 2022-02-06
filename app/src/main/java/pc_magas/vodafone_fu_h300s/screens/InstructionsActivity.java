c8535494f8c0f89f282e7f05d4403a0ac1daed9a
Dimitrios <pcmagas@disroot.org>
2022-02-06 23:26:34 +0200

    Closing Activity After Going Back

--- app/src/main/java/pc_magas/vodafone_fu_h300s/screens/InstructionsActivity.java
+++ app/src/main/java/pc_magas/vodafone_fu_h300s/screens/InstructionsActivity.java
@@ -1,10 +1,12 @@
 package pc_magas.vodafone_fu_h300s.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -39,11 +41,20 @@ public class InstructionsActivity extends AppCompatActivity {
         }.init(this));
     }
 
-    public void connectionForm(){
+    public void connectionForm() {
         Intent connect_activity = new Intent(this, ConnectIntoRouterActivity.class);
         Intent thisActivityItnent = this.getIntent();
         String router_url = thisActivityItnent.getStringExtra("router_url");
         connect_activity.putExtra("router_url",router_url);
         startActivity(connect_activity);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
